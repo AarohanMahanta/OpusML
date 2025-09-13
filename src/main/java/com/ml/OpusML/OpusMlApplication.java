867d40ab8b992806c67f51281d07d92f4ce6c773
aarohanmahanta <go57fim@mytum.de>
2025-09-13 21:18:08 +0530

    Initial commit

---
+++ src/main/java/com/ml/OpusML/OpusMlApplication.java
@@ -0,0 +1,13 @@
+package com.ml.OpusML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpusMlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpusMlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ml/OpusML/OpusMlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ml.OpusML;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpusMlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
