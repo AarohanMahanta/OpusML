933241ef85ee22549925dbbbfbfd16a5e5cd26e9
AarohanMahanta <aarohanmahanta@gmail.com>
2025-09-20 17:59:30 +0530

    feat: add endpoints to search and analyse tracks in Controller

--- src/main/java/com/ml/OpusML/rest/controller/OpusMLController.java
+++ src/main/java/com/ml/OpusML/rest/controller/OpusMLController.java
@@ -1,25 +1,33 @@
 package com.ml.OpusML.rest.controller;
 
+import com.ml.OpusML.service.SpotifyService;
 import opusml.OpusMLServiceGrpc;
+import opusml.Spotify;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/spotify")
 public class OpusMLController {
 
-    //Log for incoming REST and gRPC calls
-    private static final Logger logger = (Logger) LoggerFactory.getLogger(OpusMLController.class);
-
-    private final OpusMLServiceGrpc.OpusMLServiceBlockingStub grpcStub;
+    private final SpotifyService spotifyService;
 
 
-    @Autowired
-    public OpusMLController(OpusMLServiceGrpc.OpusMLServiceBlockingStub grpcStub) {
-        this.grpcStub = grpcStub;
+    public OpusMLController(SpotifyService spotifyService) {
+        this.spotifyService = spotifyService;
+    }
+
+
+    @GetMapping("/search")
+    public Spotify.SearchResponse searchTracks(@RequestParam String query, @RequestParam(defaultValue = "10") int limit) {
+        return spotifyService.searchTracks(query, limit);
+    }
+
+    @GetMapping("/analyze/{id}")
+    public Spotify.AnalyzeResponse analyzeTrack(@PathVariable("id") String trackId) {
+        return spotifyService.analyzeTrack(trackId);
     }
 }
