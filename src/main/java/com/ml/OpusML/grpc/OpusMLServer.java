d80f37e1b3ac047b6fb445cd9d7460a4d9413763
AarohanMahanta <aarohanmahanta@gmail.com>
2025-10-11 11:50:12 +0200

    feat: add gRPC Server

---
+++ src/main/java/com/ml/OpusML/grpc/OpusMLServer.java
@@ -0,0 +1,16 @@
+package com.ml.OpusML.grpc;
+
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+public class OpusMLServer {
+    public static void main(String[] args) throws Exception {
+        Server server = ServerBuilder.forPort(50051)
+                .addService(new OpusMLServiceImpl())
+                .build()
+                .start();
+
+        System.out.println("OpusML gRPC Server started on port 50051");
+        server.awaitTermination();
+    }
+}
