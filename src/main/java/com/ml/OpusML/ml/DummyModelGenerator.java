2905b7072af0bad670070f6f034147898c444d97
AarohanMahanta <aarohanmahanta@gmail.com>
2025-10-14 21:29:14 +0200

    style: formatting

--- src/main/java/com/ml/OpusML/ml/DummyModelGenerator.java
+++ src/main/java/com/ml/OpusML/ml/DummyModelGenerator.java
@@ -8,21 +8,17 @@ import weka.core.SerializationHelper;
 
 import java.util.ArrayList;
 
-/**
- * This is a placeholder Weka model aimed to load successfully and return some dummy predictions when called to test
- * the functionality of the application.
- */
 public class DummyModelGenerator {
     public static void main(String[] args) throws Exception {
-        //First define features
+        // Define features
         ArrayList<Attribute> attributes = new ArrayList<>();
-        attributes.add(new Attribute("tempo"));
         attributes.add(new Attribute("energy"));
+        attributes.add(new Attribute("tempo"));
         attributes.add(new Attribute("valence"));
         attributes.add(new Attribute("acousticness"));
         attributes.add(new Attribute("instrumentalness"));
 
-        //Define class attribute (moods)
+        // Define class attribute (moods)
         ArrayList<String> classValues = new ArrayList<>();
         classValues.add("Calm");
         classValues.add("Happy");
@@ -31,29 +27,28 @@ public class DummyModelGenerator {
         Attribute classAttr = new Attribute("mood", classValues);
         attributes.add(classAttr);
 
-        //Create empty dataset
+        // Create dataset
         Instances dataset = new Instances("MoodDataset", attributes, 0);
         dataset.setClassIndex(dataset.numAttributes() - 1);
 
-        //Dummy instances for training purposes
-        double[] vals = new double[dataset.numAttributes()];
-        vals[0] = 120;     //tempo
-        vals[1] = 0.8;     //energy
-        vals[2] = 0.7;     //valence
-        vals[3] = 0.1;     //acousticness
-        vals[4] = 0.0;     //instrumentalness
-        vals[5] = classValues.indexOf("Happy"); //class label
+        // Dummy training data (few varied instances)
+        double[][] data = {
+                {0.2, 60, 0.3, 0.9, 0.1, classValues.indexOf("Calm")},
+                {0.8, 130, 0.8, 0.1, 0.0, classValues.indexOf("Happy")},
+                {0.3, 70, 0.2, 0.8, 0.3, classValues.indexOf("Sad")},
+                {0.9, 150, 0.9, 0.05, 0.0, classValues.indexOf("Energetic")}
+        };
 
-        dataset.add(new DenseInstance(1.0, vals));
+        for (double[] vals : data) {
+            dataset.add(new DenseInstance(1.0, vals));
+        }
 
-        //Train a dummy RandomForest classifier on empty dataset
+        // Train classifier
         RandomForest classifier = new RandomForest();
         classifier.buildClassifier(dataset);
-        //Will not actually learn, but can classify dummy instances
 
-        //Save the model
+        // Save model
         SerializationHelper.write("src/main/resources/models/mood_model.model", classifier);
-
-        System.out.println("Dummy Weka model created!");
+        System.out.println("Weka mood model trained & saved successfully.");
     }
 }
