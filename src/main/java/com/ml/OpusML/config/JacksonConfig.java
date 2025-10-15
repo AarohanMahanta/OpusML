56b95be350969bcae3b5e753808b25aaf2a62cf1
AarohanMahanta <aarohanmahanta@gmail.com>
2025-10-15 21:37:33 +0200

    feat: add jackson protobuf module to handle automatic JSON serialization and deserialization of gRPC / Protobuf messages

---
+++ src/main/java/com/ml/OpusML/config/JacksonConfig.java
@@ -0,0 +1,16 @@
+package com.ml.OpusML.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hubspot.jackson.datatype.protobuf.ProtobufModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new ProtobufModule());
+        return mapper;
+    }
+}
