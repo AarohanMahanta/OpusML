efd27b9b761f1ebc5842e762cde73a787d595968
AarohanMahanta <aarohanmahanta@gmail.com>
2025-10-20 10:07:17 +0200

    docs: add documentation for OpusMLController endpoints

--- src/main/java/com/ml/OpusML/rest/controller/OpusMLController.java
+++ src/main/java/com/ml/OpusML/rest/controller/OpusMLController.java
@@ -17,11 +17,23 @@ public class OpusMLController {
         this.spotifyService = spotifyService;
     }
 
+    /**
+     * Endpoint takes name of composer/piece from user with number of tracks to show.
+     * @param query: name of composer/piece, keyword that is used fetch tracks from spotify/database.
+     * @param limit: maximum number of tracks to be fetched, defaulting to 5 for no input.
+     * @return is a SearchResponse DTO.
+     */
     @GetMapping("/search")
     public SearchResponse searchTracks(@RequestParam String query, @RequestParam(defaultValue = "5") int limit) {
         return spotifyService.searchTracks(query, limit);
     }
 
+    /**
+     * Endpoint takes ID of track with the top number of recommended tracks to show.
+     * @param trackId: ID of track selected by user, for which recommendations are to be shown.
+     * @param topK: maximum number of recommendations to be shown i.e. the top K number of tracks sorted by similarity.
+     * @return is a RecommendationResponse DTO.
+     */
     @PostMapping("/recommend")
     public SpotifyService.RecommendationResponse recommendTracks(@RequestParam String trackId, @RequestParam(defaultValue = "3") int topK) {
         return spotifyService.recommendTracks(trackId, topK);
